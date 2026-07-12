56e0fefb1ee250d226bc2898b036e7f7182282e6
Kellyson Santos <kellyson@MacBook-Pro-de-Kellyson.local>
2026-07-12 18:17:09 -0300

    feat: Create Task frontend

---
+++ frontend/src/app/src/features/tasks/create-task/create-task.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'create-task',
+  imports: [],
+  templateUrl: './create-task.html',
+  styleUrl: './create-task.css',
+})
+export class CreateTask {
+
+}
